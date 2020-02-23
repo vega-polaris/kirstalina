b3ba5152653c191e185023f54f0099cf2e578a23
Featherweight <vais.emma@gmail.com>
2020-02-23 00:55:30 -0500

    created both portfolio components

---
+++ client/components/singlePortfolioRow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+const SinglePortfolioRow = props => {
+  console.log('rendering single port row');
+  const {row} = props;
+  return (
+    <TableRow>
+      <TableCell component="th" scope="row">
+        {row.ticker}
+      </TableCell>
+      <TableCell align="right">{row.amount}</TableCell>
+      <TableCell align="right">{row.curPrice * 0.001 * row.amount}</TableCell>
+    </TableRow>
+  );
+};
+
+export default SinglePortfolioRow;
